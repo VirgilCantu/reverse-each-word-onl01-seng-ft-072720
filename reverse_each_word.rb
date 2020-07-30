81dd72ae8b1fefb341b21f17871af1730a5fcb02
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-30 01:11:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def reverse_each_word(sentence)
 reversed_sentence = []
-sentence.split
-# binding.pry
+sentence_array = sentence.split
+binding.pry
 sentence.each do |word|
   reversed_sentence << word
   end
